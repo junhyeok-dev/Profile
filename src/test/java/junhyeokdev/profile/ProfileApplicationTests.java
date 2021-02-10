ad8c5180085b439ccccc54e603a46db7b0c2f8e4
Junhyeok Yun <junhyeok.dev@gmail.com>
2021-02-10 22:44:51 +0900

    init: Initial commit

---
+++ src/test/java/junhyeokdev/profile/ProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package junhyeokdev.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
